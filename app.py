ad0f722f3fc36c3875031eda5aa6b4528f5ba132
raghu-05 <arjaraghuveer@gmail.com>
2025-01-05 21:56:58 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,183 @@
+import cv2
+import os
+from flask import Flask, request, render_template
+from datetime import date, datetime
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+import joblib
+
+app = Flask(__name__)
+attendance_taken = False
+
+nimgs = 10
+
+imgBackground = cv2.imread("background.png")
+
+datetoday = date.today().strftime("%m_%d_%y")
+datetoday2 = date.today().strftime("%d-%B-%Y")
+
+face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+if not os.path.isdir('Attendance'):
+    os.makedirs('Attendance')
+if not os.path.isdir('static'):
+    os.makedirs('static')
+if not os.path.isdir('static/faces'):
+    os.makedirs('static/faces')
+if f'Attendance-{datetoday}.csv' not in os.listdir('Attendance'):
+    with open(f'Attendance/Attendance-{datetoday}.csv', 'w') as f:
+        f.write('Name,Roll,Time')
+
+def totalreg():
+    return len(os.listdir('static/faces'))
+
+def extract_faces(img):
+    try:
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        face_points = face_detector.detectMultiScale(gray, 1.1, 4, minSize=(30, 30))
+        return face_points
+    except Exception as e:
+        print(f"Error in face detection: {e}")
+        return []
+
+def identify_face(facearray):
+    try:
+        model = joblib.load('static/face_recognition_model.pkl')
+        return model.predict(facearray)
+    except Exception as e:
+        print(f"Error in face identification: {e}")
+        return ["Unknown"]
+
+def train_model():
+    faces = []
+    labels = []
+    userlist = os.listdir('static/faces')
+    for user in userlist:
+        user_folder = os.path.join('static/faces', user)
+        for imgname in os.listdir(user_folder):
+            img = cv2.imread(os.path.join(user_folder, imgname))
+            if img is not None:
+                resized_face = cv2.resize(img, (50, 50))
+                faces.append(resized_face.ravel())
+                labels.append(user)
+    faces = np.array(faces)
+    if len(faces) > 0:
+        knn = KNeighborsClassifier(n_neighbors=5)
+        knn.fit(faces, labels)
+        joblib.dump(knn, 'static/face_recognition_model.pkl')
+    else:
+        print("No faces to train.")
+
+def extract_attendance():
+    try:
+        df = pd.read_csv(f'Attendance/Attendance-{datetoday}.csv')
+        names = df['Name']
+        rolls = df['Roll']
+        times = df['Time']
+        return names, rolls, times, len(df)
+    except Exception as e:
+        print(f"Error extracting attendance: {e}")
+        return [], [], [], 0
+
+def add_attendance(name):
+    username = name.split('_')[0]
+    userid = name.split('_')[1]
+    current_time = datetime.now().strftime("%H:%M:%S")
+
+    try:
+        df = pd.read_csv(f'Attendance/Attendance-{datetoday}.csv')
+        if int(userid) not in list(df['Roll']):
+            with open(f'Attendance/Attendance-{datetoday}.csv', 'a') as f:
+                f.write(f'\n{username},{userid},{current_time}')
+    except Exception as e:
+        print(f"Error adding attendance: {e}")
+
+def getallusers():
+    try:
+        userlist = os.listdir('static/faces')
+        names = []
+        rolls = []
+        for i in userlist:
+            name, roll = i.split('_')
+            names.append(name)
+            rolls.append(roll)
+        return userlist, names, rolls, len(userlist)
+    except Exception as e:
+        print(f"Error getting users: {e}")
+        return [], [], [], 0
+
+@app.route('/')
+def home():
+    names, rolls, times, l = extract_attendance()
+    return render_template('home.html', names=names, rolls=rolls, times=times, l=l, totalreg=totalreg(), datetoday2=datetoday2)
+
+@app.route('/start', methods=['GET'])
+def start():
+    names, rolls, times, l = extract_attendance()
+
+    if not os.path.isfile('static/face_recognition_model.pkl'):
+        return render_template('home.html', names=names, rolls=rolls, times=times, l=l, totalreg=totalreg(), datetoday2=datetoday2, mess='There is no trained model in the static folder. Please add a new face to continue.')
+
+    ret = True
+    cap = cv2.VideoCapture(0)
+    while ret:
+        ret, frame = cap.read()
+        if ret and len(extract_faces(frame)) > 0:
+            (x, y, w, h) = extract_faces(frame)[0]
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (86, 32, 251), 1)
+            cv2.rectangle(frame, (x, y), (x+w, y-40), (86, 32, 251), -1)
+            face = cv2.resize(frame[y:y+h, x:x+w], (50, 50))
+            identified_person = identify_face(face.reshape(1, -1))[0]
+            add_attendance(identified_person)
+            cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 1)
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(50,50,255),2)
+            cv2.rectangle(frame,(x,y-40),(x+w,y),(50,50,255),-1)
+            cv2.putText(frame, f'{identified_person}', (x,y-15), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 1)
+            cv2.rectangle(frame, (x,y), (x+w, y+h), (50,50,255), 1)
+        imgBackground[162:162 + 480, 55:55 + 640] = frame
+        cv2.imshow('Attendance', imgBackground)
+        if cv2.waitKey(1) == 27:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+    names, rolls, times, l = extract_attendance()
+    return render_template('home.html', names=names, rolls=rolls, times=times, l=l, totalreg=totalreg(), datetoday2=datetoday2)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    newusername = request.form['newusername']
+    newuserid = request.form['newuserid']
+    userimagefolder = os.path.join('static/faces', f'{newusername}_{newuserid}')
+    if not os.path.isdir(userimagefolder):
+        os.makedirs(userimagefolder)
+    i, j = 0, 0
+    cap = cv2.VideoCapture(0)
+    while 1:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        faces = extract_faces(frame)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 20), 2)
+            cv2.putText(frame, f'Images Captured: {i}/{nimgs}', (30, 30),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 20), 2, cv2.LINE_AA)
+            if j % 5 == 0:
+                name = f'{newusername}_{i}.jpg'
+                cv2.imwrite(os.path.join(userimagefolder, name), frame[y:y+h, x:x+w])
+                i += 1
+            j += 1
+        if j >= nimgs * 5:
+            break
+        cv2.imshow('Adding new User', frame)
+        if cv2.waitKey(1) == 27:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+    print('Training Model')
+    train_model()
+    names, rolls, times, l = extract_attendance()
+    return render_template('home.html', names=names, rolls=rolls, times=times, l=l, totalreg=totalreg(), datetoday2=datetoday2)
+
+if __name__ == '__main__':
+    app.run(debug=True)
